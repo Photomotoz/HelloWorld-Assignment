f7e76ddf32d203951e6c3c46b3bea3f1a9bb2444
Dmitry Tarasev <>
2018-09-17 14:13:17 -0400

    Added URL redirects to registration page from '/' and '/SimplePage'

--- HelloWorldDjango/urls.py
+++ HelloWorldDjango/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='registration')),
     path('SimplePage/', include('SimplePage.urls')),
 ]
