151047e526b12561a1a1ce0e0d4c2c21612f0696
Dmitry Tarasev <>
2018-09-18 15:02:00 -0400

    Setting secret key to env variable, a few commits too late....

--- HelloWorldDjango/settings.py
+++ HelloWorldDjango/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n(xb6^l9)r7r=7oo^@6by6+5syk9_rr!y=0g)i9b1jaf%j=!3k'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
